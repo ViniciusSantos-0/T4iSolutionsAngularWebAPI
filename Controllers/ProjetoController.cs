2bfbd16722286eab8c59d7266967f3b96479a658
Vinicius Santos <viniciushacrinha1@gmail.com>
2021-08-30 11:45:56 -0300

    Terminando o CRUD de Projetos

--- Controllers/ProjetoController.cs
+++ Controllers/ProjetoController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using T4i_WebAPI.Data;
+using T4i_WebAPI.model;
 
 namespace T4i_WebAPI.Controllers
 {
@@ -15,10 +16,10 @@ namespace T4i_WebAPI.Controllers
             _repo = repo;
         }
         [HttpGet]
-        public async Task <IActionResult> Get()
+        public async Task<IActionResult> Get()
         {
             try
-            {   
+            {
                 var result = await _repo.GetAllProjetosAsync(true);
 
                 return Ok(result);
@@ -29,13 +30,13 @@ namespace T4i_WebAPI.Controllers
             }
 
         }
-         [HttpGet("{projetoId}")]
-        public async Task <IActionResult> GetByProjetoId(int projetoId)
+        [HttpGet("{projetoId}")]
+        public async Task<IActionResult> GetByProjetoId(int projetoId)
         {
             try
-            {   
+            {
                 var result = await _repo.GetProjetoAsyncById(projetoId, true);
-                
+
                 return Ok(result);
             }
             catch (Exception ex)
@@ -43,6 +44,78 @@ namespace T4i_WebAPI.Controllers
                 return BadRequest($"Erro: {ex.Message}");
             }
 
+        }
+        [HttpGet("works/{worksId}")]
+        public async Task<IActionResult> GetByWorksId(int worksId)
+        {
+            try
+            {
+                var result = await _repo.GetProjetosAsyncByWorksId(worksId, false);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Projeto model)
+        {
+            try
+            {
+                _repo.Add(model);
+
+                if (await _repo.SaveChangesAsync())
+                {
+                    return Ok(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+            return BadRequest();
+        }
+        [HttpPut("{projetoId}")]
+        public async Task<IActionResult> Put(int projetoId, Projeto model)
+        {
+            try
+            {
+                var projeto = await _repo.GetProjetoAsyncById(projetoId, false);
+                if (projeto == null) return NotFound("Projeto não encontrado");
+                _repo.Update(model);
+
+                if (await _repo.SaveChangesAsync())
+                {
+                    return Ok(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+            return BadRequest();
+        }
+         [HttpDelete("{projetoId}")]
+        public async Task<IActionResult> delete(int projetoId)
+        {
+            try
+            {
+                var projeto = await _repo.GetProjetoAsyncById(projetoId, false);
+                if (projeto == null) return NotFound();
+                _repo.Delete(projeto);
+
+                if (await _repo.SaveChangesAsync())
+                {
+                    return Ok("Deletado com sucesso!");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+            return BadRequest();
         }
     }
 }
